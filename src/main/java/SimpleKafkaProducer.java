7a6ffe57c47acd2c4025de8ef84e430907624c51
Rohith <rohith@mtccrm.com>
2024-07-13 20:34:46 -0500

    First changes via git

--- src/main/java/SimpleKafkaProducer.java
+++ src/main/java/SimpleKafkaProducer.java
@@ -6,6 +6,8 @@ import java.util.Properties;
 
 public class SimpleKafkaProducer {
     public static void main(String[] args) {
+
+        System.out.println("Helooo 123");
         String topicName = "test-topic";
 
         // Set Kafka producer properties
